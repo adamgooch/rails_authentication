ba92ae8e9d8db26d9a54e7ff2bdd9b3dd0b8f059
Adam Gooch <adamgooch@acm.org>
2012-10-14 20:53:40 -0400

    authentication without password added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def index
+
   end
 
   def new
@@ -10,11 +11,28 @@ class UsersController < ApplicationController
     @user = User.new
     @user.email = params[:user][:email]
     @user.password_digest = params[:user][:password]
-    @user.save
-    redirect_to @user
+    if @user.save
+      redirect_to @user
+      flash[:success] = 'Good Job!'
+    else
+      render :new
+      flash[:error] = 'What Happened?'
+    end
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  def authenticate
+    user = User.find_by_email(params[:email])
+    if user
+      flash[:success] = 'Nice!'
+      redirect_to user
+    else
+      flash[:error] = 'Failed Log In'
+      render :index
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Pbkdf2Test::Application.routes.draw do
   resources :users
+  match "/authenticate" => "users#authenticate"
 end
