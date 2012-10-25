8df01b4dd11b977992e51f86ebe70a2d3c2a9178
Adam Gooch <agooch@8thlight.com>
2012-10-24 20:44:23 -0400

    BCrypt implemented for creating users and authenticating them. Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,3 @@
-require 'pbkdf2'
-
 class UsersController < ApplicationController
   def index
 
@@ -10,20 +8,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    @user.email = params[:user][:email]
-    if params[:user][:password] == params[:user][:password_confirmation]
-      @user.salt = make_salt
-      @user.password_digest = encrypt(params[:user][:password], @user.salt)
-      if @user.save
-        flash[:success] = 'Good Job!'
-        redirect_to @user
-      else
-        flash.now[:error] = 'What Happened?'
-        render :new
-      end
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = 'Good Job!'
+      redirect_to @user
     else
-      flash.now[:error] = 'Password conirmation does not match password'
+      flash.now[:error] = 'What Happened?'
       render :new
     end
   end
@@ -34,28 +24,13 @@ class UsersController < ApplicationController
 
   def authenticate
     user = User.find_by_email(params[:email])
-    if user && user.password_digest == encrypt(params[:password], user.salt)
+    if user.try(:authenticate, params[:password])
       flash[:success] = 'Nice!'
       redirect_to user
     else
       flash.now[:error] = 'Failed Log In'
       render :index
     end
-  end
-
-  private
-
-  def encrypt(clear_text, salt)
-    derived_key = PBKDF2.new do |key|
-      key.password = clear_text
-      key.salt = salt
-      key.iterations = 10000
-    end
-    return derived_key.hex_string
-  end
-
-  def make_salt
-    return SecureRandom.hex
   end
 
 end
